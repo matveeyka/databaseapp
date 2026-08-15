1b93b06e80769a2f21d7e18ad475cfbe357c79c3
Matvei Myasin <thecooldit111@gmail.com>
2026-08-15 15:56:43 +0300

    fix: removed database(it no longer needed)

--- dbapp/models.py
+++ dbapp/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
     header = models.CharField(max_length=50)
     desc = models.CharField(max_length=1000)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
