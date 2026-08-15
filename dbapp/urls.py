da11f152d8d68573ef87e86415b65212990580b4
Matvei Myasin <thecooldit111@gmail.com>
2026-08-15 15:58:44 +0300

    feat: added new url paths

--- dbapp/urls.py
+++ dbapp/urls.py
@@ -5,5 +5,8 @@ urlpatterns = [
     path('', views.index),
     path('post', views.post_page),
     path('postdb/', views.postdb),
-    path('api-info', views.api_info)
+    path('api-info', views.api_info),
+    path('register', views.register, name='register'),
+    path('login', views.login_view, name='login'),
+    path('logout', views.logout_view, name='logout'),
 ]
