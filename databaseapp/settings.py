35ad41d41eb70456eb971261f03ea4e65a62f2c0
Matvei Myasin <thecooldit111@gmail.com>
2026-08-15 14:46:12 +0300

    fix: made settings.py safer

--- databaseapp/settings.py
+++ databaseapp/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_0m23f-k*_j=oxgm+ni@bc3e0ea()432w#gmsb$qb#&%qz9$9+'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
