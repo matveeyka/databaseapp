6e857de541d04e5c5f0850645c507b47bcc058cc
Matvei Myasin <thecooldit111@gmail.com>
2026-08-15 16:07:51 +0300

    fix: added __str__

--- dbapp/models.py
+++ dbapp/models.py
@@ -5,3 +5,6 @@ class Post(models.Model):
     header = models.CharField(max_length=50)
     desc = models.CharField(max_length=1000)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.header
