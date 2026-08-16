a71efc9787d954efe5b00da1e7e5985cc2023419
Matvei Myasin <thecooldit111@gmail.com>
2026-08-16 11:26:39 +0300

    fix: edited post user arguments

--- dbapp/models.py
+++ dbapp/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     header = models.CharField(max_length=50)
     desc = models.CharField(max_length=1000)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.header
